62a9828761ab5ea0864ee0b79dd50b30aa248db1
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:17:02 +0200

    Refactor Mammal.h with proper include guards and class definition
    Updated include guards and added includes for Assignment.h and iostream. Defined the Mammal class with a constructor.

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,19 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+#ifndef MAMMAL_H_INCLUDED
+#define MAMMAL_H_INCLUDED
+#include "Assignment.h"
+using namespace std;
+#include <iostream>
+class Mammal: public Animal{
+private:
+    string furColor;
+public:
+    Mammal(string name , int age , bool isHungary , string furColor):Animal(name , age , isHungary){
+       this->furColor=furColor;
+    }
+    Mammal();
+};
 
-#include "Animal.h"
-
-//define mammal class here
 
 
 
-
-
-
-#endif
+#endif // MAMMAL_H_INCLUDED
