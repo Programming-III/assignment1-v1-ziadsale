a311c424e56f159ac6df4cb2ca91d8ed1adc66ff
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:13:12 +0200

    Refactor Animal class and fix issues
    Updated Animal class definition and added methods.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,33 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
-#include <string>
+#ifndef ASSIGNMENT_H_INCLUDED
+#define ASSIGNMENT_H_INCLUDED
 using namespace std;
+#include <iostream>
+class Animal{
+protected:
+    string name;
+    int age;
+    bool isHungary;
+public:
+    Animal(string name , int age , bool isHungary){
+        this->name=name;
+        this->age=age;
+        this->isHungary=isHungary;
+    }
+    Animal();
+    void display(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age :"<<age<<endl;
+        cout<<"Is Hungary :"<<isHungary<<endl;
+    }
+    void feed(){
+        if(isHungary ="yes"){
+            cout<<"I am Hungary"<<endl;
+        }else
+        cout<< "I am full"<<endl;
+    }
+};
 
-//define animal class here 
 
 
+#endif // ASSIGNMENT_H_INCLUDED
 
-#endif
