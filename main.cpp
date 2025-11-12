f400a7336aa2b7e2f662cfbeebe50a31ba945f94
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:18:57 +0200

    Refactor main.cpp with new class implementations
    Updated includes and main function logic to use new classes.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
-#include <iostream>
 using namespace std;
-
-
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
+#include <iostream>
+#include "Assignment.h"
+#include "mammal.h"
+#include "bird.h"
+#include "reptile.h"
+#include "enclousure.h"
+#include "visitor.h"
+int main(){
+   Enclosure e(1);
+   Mammal* m1 = new Mammal("Lion" , 5 , "yes");
+   Bird* b1= new Bird ("Parrot" , 2 ,"no" );
+   Reptile* r1=new Reptile("Snake" , 3 , "Venomous" , "Yes")
+   m1->display();
+   b1->display();
+   r1->display();
+   Visitor v1("Sarah Ali" , 3)
+   v1.displayInfo();
     return 0;
+
 }
+
+
+
