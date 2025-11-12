036f6af492e708b90b9d56f9fef4ad82ec1f6231
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:16:30 +0200

    Update Enclosure class with animal management
    Refactor header guard and add methods for managing animals.

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,49 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+#ifndef ENCLOUSURE_H_INCLUDED
+#define ENCLOUSURE_H_INCLUDED
+using namespace std;
+#include <iostream>
+#include "Assignment.h"
 
-#include "Animal.h"
+class Enclosure {
+private:
+    Animal** animals;
+    int capacity;
+    int currentCount;
 
-//define enclosure class here 
+public:
+    Enclosure(int capacity) {
+      this->capacity = capacity;
+        currentCount = 0;
+        animals = new Animal*[capacity];
+    }
+
+
+    void addAnimal(Animal* a) {
+        if (currentCount < capacity) {
+            animals[currentCount++] = a;
+            cout << "Animal added successfully ";
+        } else {
+            cout << "Enclosure is full! Cannot add more animals.\n";
+        }
+    }
+
+
+    void displayAnimals() const {
+        cout << "Animals in the enclosure: ";
+        for (int i = 0; i < currentCount; i++) {
+            animals[i]->display();
+        }
+        if (currentCount == 0)
+            cout << "No animals in the enclosure";
+    }
+
+
+    Enclosure() ;
+
+
+};
 
 
 
 
-
-
-
-
-#endif
+#endif // ENCLOUSURE_H_INCLUDED
