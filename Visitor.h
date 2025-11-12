025bd6b4d58f559075e912d160721e2cf61e61e8
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:17:57 +0200

    Refactor Visitor class header file

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,24 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
-#include <string>
+#ifndef VISITOR_H_INCLUDED
+#define VISITOR_H_INCLUDED
 using namespace std;
+#include <iostream>
+class Visitor{
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+    Visitor(string visitorName , int ticketsBought){
+        this->visitorName=visitorName;
+        this->ticketsBought=ticketsBought;
+    }
+    Visitor();
+    ~Visitor();
+        void displayInfo(){
+        cout<<"Visitor Name :"<<visitorName<<endl;
+        cout<<"Tickets Bought :"<<ticketsBought<<endl;
+    }
+};
 
-//define visitor class here
 
 
-
-
-
-
-
-#endif
+#endif // VISITOR_H_INCLUDED
