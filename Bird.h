925ef47b52fa7b603ad09b37f9c25115cd3283b8
Ziad Saleh <105130973+ziadsale@users.noreply.github.com>
2025-11-12 15:15:34 +0200

    Refactor Bird.h with include guards and includes
    Updated include guards and added necessary includes.

--- Bird.h
+++ Bird.h
@@ -1,14 +1,18 @@
-#ifndef BIRD_H
-#define BIRD_H
+#ifndef BIRD_H_INCLUDED
+#define BIRD_H_INCLUDED
+using namespace std;
+#include <iostream>
+#include "Assignment.h"
+class Bird:public Animal{
+private:
+    float wingSpan;
+public:
+    Bird(string name , int age , bool isHungary , float wingSpan):Animal(name , age , isHungary){
+       this->wingSpan=wingSpan;
+    }
+    Bird();
+};
 
-#include "Animal.h"
-
-//define class bird here 
 
 
-
-
-
-
-
-#endif
+#endif // BIRD_H_INCLUDED
